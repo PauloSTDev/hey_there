fba8d0b4fa654deafd887572cd3871fd9a43a447
Paulo Santos <compasso-paulo.santos@ailos.coop.br>
2023-02-28 09:33:29 -0300

    Add docs

--- lib/src/features/change_text_color/usecase/change_text_color_by_background_color_usecase_impl.dart
+++ lib/src/features/change_text_color/usecase/change_text_color_by_background_color_usecase_impl.dart
@@ -6,17 +6,25 @@ class ChangeTextColorByBackgroundColorUsecaseImpl
   @override
   Color changeTextColorByBackgroundColor(Color colorText, int colorAlpha,
       int colorRed, int colorBlue, int colorGreen) {
-    final int averageColorAlpha = 255 ~/ 2;
-    final int averageRgb = averageColorAlpha * 3;
-    final int averageColors = colorRed + colorBlue + colorGreen;
+    ///Average max of Alpha Color, we will use to know if colorAlpha is above this average
+    final int averageMaxColorAlpha = 255 ~/ 2;
 
-    if (colorAlpha >= averageColorAlpha) {
-      if (averageColors >= averageRgb) {
+    ///Average max of rgb, we will use to compare with summedRGB
+    final int averageMaxRgb = (255 * 3) ~/ 2;
+
+    ///Sum of RGB (Ramdomly numbers)
+    final int summedRGB = colorRed + colorBlue + colorGreen;
+
+    ///Compare if colorAlpha is above or equal your average max
+    if (colorAlpha >= averageMaxColorAlpha) {
+      ///Compare if summedColors is above or equal your average max
+      if (summedRGB >= averageMaxRgb) {
         return colorText = Colors.black;
       } else {
-        if (colorRed + colorBlue >= averageRgb ||
-            colorRed + colorGreen >= averageRgb ||
-            colorGreen + colorBlue >= averageRgb) {
+        ///Compare if at least two colors from RGB are above or equal the average max
+        if (colorRed + colorBlue >= averageMaxRgb ||
+            colorRed + colorGreen >= averageMaxRgb ||
+            colorGreen + colorBlue >= averageMaxRgb) {
           return colorText = Colors.black;
         }
         return colorText = Colors.white;
